ddc61b7ee18563982769d37f43f7a5b4560e395e
Doni Winata <doni.winata@traveloka.com>
2019-12-04 19:11:57 +0700

    init project

---
+++ androidAppModule0/src/main/java/androidAppModule0packageJava0/Foo0.java
@@ -0,0 +1,33 @@
+package androidAppModule0packageJava0;
+
+public class Foo0 {
+  public void foo0() {
+    new androidAppModule6packageKt0.Foo0().foo4();
+    new androidAppModule2packageKt0.Foo0().foo4();
+    new module0packageKt0.Foo0().foo4();
+    new androidAppModule4packageKt0.Foo0().foo4();
+    new androidAppModule8packageKt0.Foo0().foo4();
+    new module1packageKt0.Foo0().foo4();
+    new androidAppModule1packageKt0.Foo0().foo4();
+    new androidAppModule9packageKt0.Foo0().foo4();
+    new androidAppModule7packageKt0.Foo0().foo4();
+    new androidAppModule3packageKt0.Foo0().foo4();
+    new androidAppModule5packageKt0.Foo0().foo4();
+  }
+
+  public void foo1() {
+    foo0();
+  }
+
+  public void foo2() {
+    foo1();
+  }
+
+  public void foo3() {
+    foo2();
+  }
+
+  public void foo4() {
+    foo3();
+  }
+}
---
+++ androidAppModule3/src/main/java/com/androidAppModule3/Activity29.java
@@ -0,0 +1,25 @@
+package com.androidAppModule3;
+
+import android.widget.ImageView;
+import java.lang.Override;
+
+public class Activity29 extends android.app.Activity {
+  ImageView imageView0;
+
+  ImageView imageView1;
+
+  ImageView imageView2;
+
+  ImageView imageView3;
+
+  ImageView imageView4;
+
+  ImageView imageView5;
+
+  @Override
+  public void onCreate(android.os.Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    new androidAppModule3packageJava0.Foo0().foo4();
+    setContentView(R.layout.androidappmodule3activity_main29);
+  }
+}
