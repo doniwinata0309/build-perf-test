8f84e7d6193d25d147bdab319677eb0f482de223
doni.winata <doni.winata@traveloka.com>
2020-10-06 17:33:07 +0700

    add ui test  on module 2

---
+++ androidAppModule2/src/androidTest/java/androidAppModule2packageJava0/ExampleInstrumentedTest2.kt
@@ -0,0 +1,24 @@
+package androidAppModule2packageJava0
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4ClassRunner::class)
+class ExampleInstrumentedTest2 {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.test.myapplication.dynamicfeature_2", appContext.packageName)
+    }
+}
